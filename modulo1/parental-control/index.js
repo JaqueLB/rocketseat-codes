dd8da80b1c756d93d6174c6b1dc00627c2cbc5e4
Jaqueline <jlemesbotaro@gmail.com>
2019-03-04 12:20:19 -0300

    Redirect if no age or age is empty string

--- modulo1/parental-control/index.js
+++ modulo1/parental-control/index.js
@@ -4,7 +4,7 @@ const app = express()
 app.use(express.urlencoded({ extended: false }))
 
 const ageMiddleware = (req, res, next) => {
-  if (!req.query.age) {
+  if (req.query.age === 'undefined' || req.query.age === '') {
     return res.redirect('/')
   }
 
@@ -12,19 +12,14 @@ const ageMiddleware = (req, res, next) => {
 }
 
 app.get('/', (req, res) => {
-  return res.json({ message: 'Hello World!' })
+  return res.json({ message: req.message })
 })
 
 app.post('/check', (req, res) => {
   const age = req.body.age
-  if (!age) {
-    return res.redirect('/')
-  }
-
   if (age >= 18) {
     return res.redirect(`/major?age=${age}`)
   }
-
   return res.redirect(`/minor?age=${age}`)
 })
 

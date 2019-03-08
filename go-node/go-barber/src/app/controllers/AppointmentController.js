d65ff0390b76e573ec239ac10290871666cab273
Jaqueline <jlemesbotaro@gmail.com>
2019-03-07 22:51:12 -0300

    Add flash message after successfully create appointment

--- go-node/go-barber/src/app/controllers/AppointmentController.js
+++ go-node/go-barber/src/app/controllers/AppointmentController.js
@@ -11,11 +11,19 @@ class AppointmentController {
     const { provider } = req.params
     const { date } = req.body
 
+    if (!date) {
+      req.flash('error', 'Escolha uma data e um horário disponível')
+
+      return res.redirect(`/app/appointments/new/${provider}`)
+    }
+
     await Appointment.create({
       user_id: id,
       provider_id: provider,
       date
     })
+
+    req.flash('success', 'Agendamento realizado com sucesso')
 
     return res.redirect('/app/dashboard')
   }

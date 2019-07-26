f34f78a010a8115fdccaa00f3b09151580acb42b
Jaqueline L. Botaro <jlemesbotaro@gmail.com>
2019-07-25 22:43:29 -0300

    Comment scoped css

--- go-react/github-repo-compare/src/App.js
+++ go-react/github-repo-compare/src/App.js
@@ -2,6 +2,12 @@ import React, { Fragment } from "react";
 import GlobalStyle from "./styles/global";
 import Main from "./pages/Main";
 
+// scoped css! it exists only here
+// const Title = styled.h1`
+//   color: #f00;
+//   font-size: 32px;
+// `;
+
 const App = () => (
   <Fragment>
     <GlobalStyle />

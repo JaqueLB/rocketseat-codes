d2370e8500f977651c7573b3f33355c0d28fb36f
Jaqueline L. Botaro <jlemesbotaro@gmail.com>
2019-08-02 21:49:17 -0300

    Make stateful component and dynamic view

--- go-react/github-repo-compare/src/components/CompareList/index.js
+++ go-react/github-repo-compare/src/components/CompareList/index.js
@@ -2,58 +2,32 @@ import React from "react";
 
 import { Container, Repository } from "./styles";
 
-const CompareList = () => (
+const CompareList = ({ repositories }) => (
   <Container>
-    <Repository>
-      <header>
-        <img
-          src="https://avatars3.githubusercontent.com/u/69631?v=4"
-          alt="Facebook Logo"
-        />
-        <strong>react</strong>
-        <small>Facebook</small>
-      </header>
+    {repositories.map(repository => (
+      <Repository>
+        <header>
+          <img src={repository.owner.avatar_url} alt={repository.owner.login} />
+          <strong>{repository.name}</strong>
+          <small>{repository.owner.login}</small>
+        </header>
 
-      <ul>
-        <li>
-          96,014 <small>stars</small>
-        </li>
-        <li>
-          96,014 <small>forks</small>
-        </li>
-        <li>
-          96,014 <small>issues</small>
-        </li>
-        <li>
-          1 day ago <small>last commit</small>
-        </li>
-      </ul>
-    </Repository>
-    <Repository>
-      <header>
-        <img
-          src="https://avatars3.githubusercontent.com/u/69631?v=4"
-          alt="Facebook Logo"
-        />
-        <strong>react</strong>
-        <small>Facebook</small>
-      </header>
-
-      <ul>
-        <li>
-          96,014 <small>stars</small>
-        </li>
-        <li>
-          96,014 <small>forks</small>
-        </li>
-        <li>
-          96,014 <small>issues</small>
-        </li>
-        <li>
-          1 day ago <small>last commit</small>
-        </li>
-      </ul>
-    </Repository>
+        <ul>
+          <li>
+            {repository.stargazers_count} <small>stars</small>
+          </li>
+          <li>
+            {repository.forks_count} <small>forks</small>
+          </li>
+          <li>
+            {repository.open_issues_count} <small>issues</small>
+          </li>
+          <li>
+            {repository.pushed_at} <small>last commit</small>
+          </li>
+        </ul>
+      </Repository>
+    ))}
   </Container>
 );
 
